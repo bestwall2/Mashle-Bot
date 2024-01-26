47274ac3f224999279aae1b4782272373277cc0a
Ahmed Dikha <85714557+bestwall2@users.noreply.github.com>
2024-01-26 05:30:12 +0100

    update Aki.py

--- commands/Aki.py
+++ commands/Aki.py
@@ -9,6 +9,6 @@ def command(input_value, thread_userid=None):
     if input_value == "__config__":
         return config
     elif input_value.startswith(config['name'] + ' ') or input_value == config['name']: 
-    	return {'messages': ["DEVELOPED BY 𝑯𝒂𝒚𝒂𝒌𝒂𝒘𝒂 𝑨𝒌𝒊𝒊"], 'image': ['commands/images/onimai-oniichan-wa-oshimai.gif']}
+    	return {'messages': ["DEVELOPED BY 𝑯𝒂𝒚𝒂𝒌𝒂𝒘𝒂 𝑨𝒌𝒊𝒊 Thanks For Using Ower Bot"], 'image': ['commands/images/onimai-oniichan-wa-oshimai.gif']}
     else:
         return {'messages': [f"Default option in {config['name']}: {config['description']}"]}
